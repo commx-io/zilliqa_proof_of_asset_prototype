8cc11dd327a2f97088e1b68c1dbcdd46e89c1242
Sven Meyer <github_dev@svenmeyer.com>
2019-12-05 15:22:27 +1100

    added getPrice_fromContract()

--- test/deploy_set_get.js
+++ test/deploy_set_get.js
@@ -6,24 +6,24 @@ const {
   getAddressFromPrivateKey,
 } = require('@zilliqa-js/crypto');
 
-const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
-// const zilliqa = new Zilliqa('http://localhost:4200');
+// const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
+const zilliqa = new Zilliqa('http://localhost:4200');
 
 // These are set by the core protocol, and may vary per-chain.
 // You can manually pack the bytes according to chain id and msg version.
 // For more information: https://apidocs.zilliqa.com/?shell#getnetworkid
 
-// const chainId = 111; // chainId for kaya local blockchain
-const chainId = 333; // chainId for developer testnet
+const chainId = 111; // chainId for kaya local blockchain
+// const chainId = 333; // chainId for developer testnet
 
 const msgVersion = 1; // current msgVersion
 const VERSION = bytes.pack(chainId, msgVersion);
 
 // Populate the wallet with an account for Testnet
-const privateKey = '447a392d41017c14ec0a1786fc46388f63e7865ec759d07bce0a0c6e2dc41b5c';
+// const privateKey = '447a392d41017c14ec0a1786fc46388f63e7865ec759d07bce0a0c6e2dc41b5c';
 
 // account 0 on kaya
-// const privateKey = 'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3';
+const privateKey = 'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3';
 
 zilliqa.wallet.addByPrivateKey(privateKey);
 
@@ -73,7 +73,7 @@ async function testBlockchain() {
     const contract = zilliqa.contracts.new(code, init);
 
     // Deploy the contract
-    const [deployTx, hello] = await contract.deploy({
+    const [deployTx, proof_ipfs] = await contract.deploy({
       version: VERSION,
       gasPrice: myGasPrice,
       gasLimit: Long.fromNumber(15000),
@@ -86,12 +86,20 @@ async function testBlockchain() {
 
     // Get the deployed contract address
     console.log('The contract address is:');
-    console.log(hello.address);
+    console.log(proof_ipfs.address);
     //Following line added to fix issue https://github.com/Zilliqa/Zilliqa-JavaScript-Library/issues/168
-    const deployedContract = zilliqa.contracts.at(hello.address);
+    const deployedContract = zilliqa.contracts.at(proof_ipfs.address);
+
+	const params_default =       {
+        // amount, gasPrice and gasLimit must be explicitly provided
+        version: VERSION,
+        amount: new BN(0),
+        gasPrice: myGasPrice,
+        gasLimit: Long.fromNumber(8000),
+	  }
 
     console.log('Calling setPrice(1000)');
-    const callTx = await hello.call(
+    const callTx = await proof_ipfs.call(
       'setPrice',
       [
         {
@@ -100,13 +108,7 @@ async function testBlockchain() {
           value: '1000',
         },
       ],
-      {
-        // amount, gasPrice and gasLimit must be explicitly provided
-        version: VERSION,
-        amount: new BN(0),
-        gasPrice: myGasPrice,
-        gasLimit: Long.fromNumber(8000),
-      },
+	  params_default
     );
 
     // Retrieving the transaction receipt (See note 2)
@@ -116,8 +118,26 @@ async function testBlockchain() {
     console.log('Getting contract state...');
     const state = await deployedContract.getState();
     console.log('The state of the contract is:');
-    console.log(JSON.stringify(state, null, 4));
-    
+	console.log(JSON.stringify(state, null, 4));
+
+	async function getPrice_fromContract() {
+		const callTxGet = await proof_ipfs.call('getPrice', [], params_default);
+		const receipt_get = callTxGet.txParams.receipt;
+		const p = receipt_get.event_logs[0].params;
+		return (p[0]['value'] ? parseInt(p[0]['value']) : 0);
+	}
+
+	console.log('calling getPrice()');
+	console.log(await getPrice_fromContract() );
+	
+	/*
+	console.log("\n\nCalling getPrice()");
+    const callTxGet = await proof_ipfs.call('getPrice', [], params_default);
+    console.log('callTxGet =', callTxGet)
+    const receipt_get = callTxGet.txParams.receipt;
+    console.log('receipt_get =', receipt_get)
+    */
+
   } catch (err) {
     console.log(err);
   }
