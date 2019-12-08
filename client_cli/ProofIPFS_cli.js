a6feae2c6c8059e6a3735cbc135ea7abca2e567d
Sven Meyer <github_dev@svenmeyer.com>
2019-12-08 19:54:29 +1100

    added addressEqual()

--- client_cli/ProofIPFS_cli.js
+++ client_cli/ProofIPFS_cli.js
@@ -1,11 +1,12 @@
 /* eslint-env node, mocha */
 
 const fs = require('fs');
-const { BN, Long, bytes, units } = require('@zilliqa-js/util');
+const { BN, Long, bytes, units, validation } = require('@zilliqa-js/util');
 const { Zilliqa } = require('@zilliqa-js/zilliqa');
-const {toBech32Address, getAddressFromPrivateKey} = require('@zilliqa-js/crypto');
+const {fromBech32Address, toBech32Address, getAddressFromPrivateKey} = require('@zilliqa-js/crypto');
 const assert = require('assert');
 
+const {addressEqual} = require ('../lib/addressEqual');
 const {ProofIPFS_API, myGasPrice} = require('../lib/ProofIPFS_API');
 
 console.log("ProofIPFS_API.myGasPrice  =", myGasPrice);
@@ -16,24 +17,33 @@ const contract_address_dev = '0x397E2d22c175c0bDF7dbF815AbD123259f37A5E6';
 network_choice = process.argv[2] || 'kaya';
 contract_address_provided = process.argv[3];
 
-//  [network, id, account_private_key]
+// [network, chain_id, privateKey, account_address, timeout_deploy, timeout_transition]
 const networks = {
-    kaya : ['http://localhost:4200',       111, 'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3'],
-    dev  : ['https://dev-api.zilliqa.com', 333, '447a392d41017c14ec0a1786fc46388f63e7865ec759d07bce0a0c6e2dc41b5c'],
-    main : ['https://api.zilliqa.com',       1, '']
+	kaya : ['http://localhost:4200',
+			111,
+			'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3',
+			'0x7bb3b0e8a59f3f61d9bff038f4aeb42cae2ecce8',
+			10000,
+			5000],
+	dev  : ['https://dev-api.zilliqa.com',
+			333,
+			'447a392d41017c14ec0a1786fc46388f63e7865ec759d07bce0a0c6e2dc41b5c',
+			'zil1pw587sm57lvlu0wlwkc3gw2sddy35au6esw589',
+			180000,
+			180000],
+    main : ['https://api.zilliqa.com',       1, '', '', 180000, 180000]
 };
 
 network_parameter = networks[network_choice.toLowerCase()]
 if (network_parameter == null)
 	throw new Error("unknown blockchain network");
-const [network, chain_id, privateKey_0] = network_parameter;  // grab parameter of network
+console.log({network_parameter});
+const [network, chain_id, privateKey, account_address, timeout_deploy, timeout_transition] = network_parameter;
 
 // Account 1 - Testnet
 // zil1amx97t2vsu59z2w8xqdvakl5cyevgfzj8mhmmz
 // 0xeEcC5F2D4C87285129c7301Acedbf4c132c42452
 const privateKey_1 = '103c44eff7bbedeefaa4b080da913d7b3b00013fabc1606651422b18ba1b1dc8';
-
-const privateKey = privateKey_0;
 
 const MSG_VERSION = 1;
 const VERSION = bytes.pack(chain_id, MSG_VERSION);
@@ -43,11 +53,27 @@ console.log({network});
 const zilliqa = new Zilliqa(network);
 
 zilliqa.wallet.addByPrivateKey(privateKey);
-
+/*
+function addressEqual(address_a, address_b) {
+	let a = '';
+	let b = '';
+	if (validation.isBech32(address_a))
+		a = fromBech32Address(address_a)
+	else
+		a = (address_a.substring(0,2) == '0x' ? address_a : '0x' + address_a);
+	if (validation.isBech32(address_b))
+		b = fromBech32Address(address_b)
+	else
+		b = (address_b.substring(0,2) == '0x' ? address_b : '0x' + address_b);
+	return a.toLowerCase() == b.toLowerCase();
+}
+*/
 const address = getAddressFromPrivateKey(privateKey);
 
 console.log(`My account address is: ${address}`);
 console.log(`My account bech32 address is: ${toBech32Address(address)}`);
+
+console.log("addressEqual(address, account_address) =", addressEqual(address, account_address));
 
 
 async function testBlockchain() {
--- test/mocha_test.js
+++ test/mocha_test.js
@@ -9,6 +9,7 @@ const { BN, Long, bytes, units } = require('@zilliqa-js/util');
 const { Zilliqa } = require('@zilliqa-js/zilliqa');
 const {toBech32Address, getAddressFromPrivateKey} = require('@zilliqa-js/crypto');
 
+const {addressEqual} = require ('../lib/addressEqual');
 const {ProofIPFS_API, myGasPrice} = require('../lib/ProofIPFS_API');
 
 //    contract_address_dev = 'zil189lz6gkpwhqtma7mlq26h5fryk0n0f0xz0hvus';
@@ -18,15 +19,27 @@ const contract_address_dev = '0x397E2d22c175c0bDF7dbF815AbD123259f37A5E6';
 network_choice = process.env.npm_config_network || 'kaya';
 console.log({network_choice});
 
+// [network, chain_id, privateKey, account_address, timeout_deploy, timeout_transition]
 const networks = {
-    kaya : ['http://localhost:4200',       111, 'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3', '0x7bb3b0e8a59f3f61d9bff038f4aeb42cae2ecce8',  10000,   5000],
-    dev  : ['https://dev-api.zilliqa.com', 333, '447a392d41017c14ec0a1786fc46388f63e7865ec759d07bce0a0c6e2dc41b5c', 'zil1pw587sm57lvlu0wlwkc3gw2sddy35au6esw589', 180000, 180000],
+    kaya : ['http://localhost:4200',
+            111,
+            'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3',
+            '0x7bb3b0e8a59f3f61d9bff038f4aeb42cae2ecce8',
+            10000,
+            5000],
+    dev  : ['https://dev-api.zilliqa.com',
+            333,
+            '447a392d41017c14ec0a1786fc46388f63e7865ec759d07bce0a0c6e2dc41b5c',
+            'zil1pw587sm57lvlu0wlwkc3gw2sddy35au6esw589',
+            180000,
+            180000],
     main : ['https://api.zilliqa.com',       1, '', '', 180000, 180000]
 };
 
 network_parameter = networks[network_choice.toLowerCase()]
 if (network_parameter == null)
-	throw new Error("unknown blockchain network");
+    throw new Error("unknown blockchain network");
+console.log({network_parameter});
 const [network, chain_id, privateKey, account_address, timeout_deploy, timeout_transition] = network_parameter;
 
 
@@ -34,11 +47,11 @@ describe('ProofIPFS', function() {
 
     describe('Connect to Zilliqa Blockchain', function() {
 
-		it('should run on node version v10', function() {
-			const node_version = process.version;
-			const ok = (node_version.substring(0,4) == 'v10.');
-			expect(ok).to.be.true;
-		})
+        it('should run on node version v10', function() {
+            const node_version = process.version;
+            const ok = (node_version.substring(0,4) == 'v10.');
+            expect(ok).to.be.true;
+        })
 
         it('should connect to the blockchain and get the right chain_id', async function() {
             zilliqa = new Zilliqa(network);
@@ -50,7 +63,8 @@ describe('ProofIPFS', function() {
         it('should have the right test account', async function() {
             zilliqa.wallet.addByPrivateKey(privateKey);
             address = getAddressFromPrivateKey(privateKey).toLowerCase();
-            expect(address).to.equal(account_address);
+            const ok = addressEqual(address, account_address);
+            expect(ok).to.be.true;
         })
 
         it('should have at least 10 ZIL in the account', async function () {
@@ -76,8 +90,8 @@ describe('ProofIPFS', function() {
 
         it('should deploy the contract', async function() {
             // https://mochajs.org/#timeouts
-            this.timeout(10000);
-            this.slow(5000);
+            this.timeout(timeout_deploy);
+            this.slow(timeout_deploy / 2);
             const MSG_VERSION = 1;
             const VERSION = bytes.pack(chain_id, MSG_VERSION);
             const myGasPrice = units.toQa('1000', units.Units.Li);
@@ -107,8 +121,8 @@ describe('ProofIPFS', function() {
 
     describe('Test the contract', function() {
 
-        this.timeout(5000);
-        this.slow(5000);
+        this.timeout(timeout_transition);
+        this.slow(timeout_transition / 2);
 
         it('should get the contract_api', async function() {
             contract_api = new ProofIPFS_API(proof_ipfs, chain_id);
@@ -150,5 +164,9 @@ describe('ProofIPFS', function() {
             expect(result).be.empty;
         })
 
+        it('should display address of deployed contract', function() {
+            console.log("\n        contract address =", proof_ipfs.address);
+            expect(true).to.be.true;
+        })
     })
 })
